dd5c56d1b68e5d4a65e9a5c89b44aeafe894516d
Jaden Walker <jadenwalker7@gmail.com>
2022-08-07 11:10:18 +0100

    First push

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -2,23 +2,28 @@ import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+import Button from '@mui/material/Button';
+import { Grid } from '@mui/material';
 
 const Popup = () => {
+  function test(){
+    // chrome.tabs.query({active: true}, function(tabs){
+    //   alert(tabs[0].url);
+    // });
+    alert(
+      'Sending Email for Job: ES-124511'
+    )
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        <img class='logo'src="https://cdn.esurv.co.uk/wp-content/uploads/2017/05/logo-esurv-white-208x80.png"/>
+        <h3 class='title'>Chrome Tool</h3>
+        <Grid display="flex" justifyContent="space-between">
+          <Button sx={{mr:2, minWidth:7}} variant='contained' onClick={test}>To B2C</Button>
+          <Button sx={{ml:2, minWidth:7}}variant='contained' onClick={test}>To Agent</Button>
+        </Grid>
       </header>
     </div>
   );
